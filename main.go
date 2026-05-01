a18d2f4495c9edca3f4bf81e68a6662d4a3d542f
else <airyelse>
2026-05-01 15:58:02 +0800

    feat: add system tray with minimize-to-tray on close

--- main.go
+++ main.go
@@ -5,10 +5,14 @@ import (
 	"log"
 
 	"github.com/wailsapp/wails/v3/pkg/application"
+	"github.com/wailsapp/wails/v3/pkg/events"
 )
 
 //go:embed frontend/dist
 var assets embed.FS
+
+//go:embed build/appicon.png
+var appIcon []byte
 
 func main() {
 	svc := NewApp()
@@ -22,17 +26,35 @@ func main() {
 			Handler: application.AssetFileServerFS(assets),
 		},
 		Windows: application.WindowsOptions{
-			AdditionalBrowserArgs: []string{"--disable-gpu"},
+			AdditionalBrowserArgs:     []string{"--disable-gpu"},
+			DisableQuitOnLastWindowClosed: true,
 		},
 	})
 
 	svc.SetApp(app)
 
-	app.Window.NewWithOptions(application.WebviewWindowOptions{
+	// System tray
+	tray := app.SystemTray.New()
+	tray.SetLabel("else-toolbox")
+	tray.SetTooltip("Else Toolbox")
+
+	menu := app.NewMenu()
+	menu.Add("显示/隐藏窗口")
+	menu.AddSeparator()
+	menu.AddRole(application.Quit)
+	tray.SetIcon(appIcon).SetMenu(menu).Run()
+
+	// Main window
+	window := app.Window.NewWithOptions(application.WebviewWindowOptions{
 		Title:            "else-toolbox",
 		Width:            1024,
 		Height:           768,
 		BackgroundColour: application.NewRGBA(27, 38, 54, 255),
+	})
+
+	// Close button hides to tray instead of quitting
+	window.OnWindowEvent(events.Common.WindowClosing, func(e *application.WindowEvent) {
+		window.Hide()
 	})
 
 	if err := app.Run(); err != nil {
